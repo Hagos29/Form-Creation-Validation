72b8f10a7eb64faa5c9e37c7dc16436077b0371e
Hagos Beyene <53543538+Hagos29@users.noreply.github.com>
2025-01-24 13:02:23 +0300

    All validation added to the project

--- script.js
+++ script.js
@@ -1,31 +1,51 @@
-const form = document.getElementById('registration-form');
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("registration-form");
 
-// Validate username
-const usernameInput = document.getElementById('username');
-const usernameError = document.getElementById('usernameError');
-if (usernameInput.Value.length < 3) {
-    usernameError.textContent = 'username must be at least 3 characters long.';
-    return;
-} else {
-    usernameError.textContent ='';
+  const feedbackDiv = document.getElementById("form-feedback");
+        
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission
+  });
+
+  //Retrieve User Inputs
+  const username = username.trim();
+  document.getElementById("username").value = username;
+ 
+  document.getElementById("email").value = email;
+  const email = email.trim();
+  document.getElementById("password").value = password;
+
+  // Validate username
+  const isValid = true;
+  const messages = [];
+
+  if (username < 3) {
+    isValid = false;
+    messages.push("username must be at least 3 characters long.");
+  }
+
+  // Validate email
+  if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(emailInput.value)) {
+    isValid = false;
+    messages.push("Please enter a valid email address");
+  }
+
+  // Validate password
+  const passwordInput = document.getElementById("password");
+  const passwordError = document.getElementById("passwordError");
+  if (password.length < 8) {
+    isValid = false;
+    messages.push("Password must be at least 6 characters long.");
+  }
+
+  //feedback message
+  feedbackDiv.style.display="block";
+  if (isValid) {
+    feedbackDiv.textContent="Registration successful!";
+    feedbackDiv.style.color="#28a745";
+}else {
+    messages.join("<br>") = feedbackDiv.innerHTML;
+    feedbackDiv.style.color = "#dc3545";
 }
 
-// Validate email
-const emailInput = document.getElementById('email');
-const emailError = document.getElementById('emailError');
-if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(emailInput.value)) {
-    emailError.textContent = 'Please enter a valid email address.';
-    return;
-} else {
-    emailError.textContent = '';
-}
-
-// Validate password
-const passwordInput = document.getElementById('password');
-const passwordError = document.getElementById('passwordError');
-if ( passwordInput.value.length < 6) {
-    passwordError.textContent = 'Password must be at least 6 characters long.';
-    return;
-} else {
-    passwordError.textContent = '';
-}
+});
